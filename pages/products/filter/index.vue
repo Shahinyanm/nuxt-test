ccd645c9f29902e78c42ddb028010e5b12f6c5fb
shahinyanm <shahinyanm@gmail.com>
2021-02-18 13:02:32 +0400

    Init project, add filters page, add Vuex filters module

---
+++ pages/products/filter/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        asdsadasdsadad
+    </div>
+</template>
+
+<script>
+  export default {
+    async asyncData({ params }) {
+      console.log(params)
+      const selectedFilters = params.filter // When calling /abc the slug will be "abc"
+      return { selectedFilters }
+    },
+    name: 'index',
+  }
+</script>
+
+<style scoped>
+
+</style>
